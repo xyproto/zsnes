c0f652515b472c4b1893c701741c8f58cc3fd91a
zsknight <zsknight@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2001-04-27 21:22:24 +0000

    Updated version # to 1.20
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@171 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/ui.asm
+++ src/ui.asm
@@ -126,7 +126,7 @@ NEWSYM	outofmemory
 SECTION .data
 NEWSYM mydebug, db '',13,10,0
 NEWSYM outofmem, db 'You don',39,'t have enough memory to run this program!',13,10,0
-%define ZVERSION '18   '
+%define ZVERSION '20   '
 ;%define ZBETA    0
 
 ; Line added by Peter Santing
