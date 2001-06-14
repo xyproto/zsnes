4b314c13b3227904b914a03df62d303761a97874
pagefault <pagefault@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2001-06-14 21:16:09 +0000

    Fixed new gfx mode checkbox
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@590 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/linux/sdlintrf.asm
+++ src/linux/sdlintrf.asm
@@ -1002,7 +1002,7 @@ db 'VARIABLE OGL WIN ',0  ;16
 ; Left side starts with Video Mode 0
 ; vid mode column = 0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6 
 NEWSYM GUI16VID, db 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1       ; 16-bit mode
-NEWSYM GUINGVID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0       ; New Graphics Mode Available
+NEWSYM GUINGVID, db 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1       ; New Graphics Mode Available
 NEWSYM GUISLVID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0       ; Scanlines
 NEWSYM GUIINVID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0       ; Interpolation
 NEWSYM GUII2VID, db 0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0       ; Interpolation(w)
