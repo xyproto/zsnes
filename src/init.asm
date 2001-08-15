73159977089578c5853d277fc6fcd50e5a2528fc
pagefault <pagefault@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2001-08-15 18:12:40 +0000

    Removed super punchout hack
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@777 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/init.asm
+++ src/init.asm
@@ -1450,34 +1450,16 @@ NEWSYM headerhack
     mov byte[hirqmode2],0
     mov byte[ENVDisable],0
 
-    EXTSYM latchxr
-
     mov esi,[romdata]
     add esi,07FC0h
     cmp dword[esi],'NEUG'
     jne .notneugier
     cmp dword[esi+4],'IER '
     jne .notneugier
-    cmp dword[esi+8],'    '
-    jne .notneugier
-    mov word[latchxr],0
     mov esi,[romdata]
     add esi,0D4150h
-    mov byte [esi],0F9h
+    mov byte[esi],0F9h
 .notneugier
-
-
-
-    mov esi,[romdata]
-    add esi,07FC0h
-    cmp dword[esi],'Supe'
-    jne .notspo
-    cmp dword[esi+4],'r Pu'
-    jne .notspo
-    cmp dword[esi+8],'nch-'
-    jne .notspo
-    mov word[latchxr],0
-.notspo
 
     mov esi,[romdata]
     add esi,0FFC0h
