495f2d1c7d1f5ee9115fc71e7d2332e68bb9adc8
pagefault <pagefault@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2001-08-29 19:49:07 +0000

    Fix for default keys in dos port
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@849 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/init.asm
+++ src/init.asm
@@ -285,12 +285,21 @@ NEWSYM numjoy,    db 0   ; number of joysticks (1 = 1, 2 = 2)
 NEWSYM pl1contrl, db 1   ; player 1 device
 NEWSYM pl1keya,   dd 0
 NEWSYM pl1keyb,   dd 0
+%ifdef __MSDOS__
+NEWSYM pl1selk,   dd 54
+NEWSYM pl1startk, dd 28
+NEWSYM pl1upk,    dd 72
+NEWSYM pl1downk,  dd 80
+NEWSYM pl1leftk,  dd 75
+NEWSYM pl1rightk, dd 77
+%else
 NEWSYM pl1selk,   dd 54
 NEWSYM pl1startk, dd 28
 NEWSYM pl1upk,    dd 200
 NEWSYM pl1downk,  dd 208
 NEWSYM pl1leftk,  dd 203 
 NEWSYM pl1rightk, dd 205
+%endif
 NEWSYM pl1Xk,     dd 31
 NEWSYM pl1Ak,     dd 45
 NEWSYM pl1Lk,     dd 32
