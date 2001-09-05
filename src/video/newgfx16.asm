3a9ad769ce28e50e01c8cfdf6841fc5b4b83401d
pagefault <pagefault@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2001-09-05 22:53:13 +0000

    Fixed BG3 priority in 16-bit gfx engines
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@883 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/video/newgfx16.asm
+++ src/video/newgfx16.asm
@@ -560,6 +560,10 @@ NEWSYM newengine16b
     mov dword[palchanged],0
 
     ; BG3 Priority
+    or [bgcmsung],ebx
+    mov [BGMS1+eax*2],ebx
+    cmp [BGMS1+eax*2-2],bx
+    je near .ngmsdraw0
     mov bl,[bg3highst]
     mov [BG3PRI+eax],bl
     cmp [BG3PRI+eax-1],bl
