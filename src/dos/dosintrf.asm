239e9c6bd1f6de7695b527b56a12f36af062bcba
stainless <stainless@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2001-10-09 05:35:42 +0000

    Added wide screen to 320x480 video modes in the DOS port.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@1059 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/dos/dosintrf.asm
+++ src/dos/dosintrf.asm
@@ -1044,16 +1044,17 @@ NEWSYM GUISLVID, db 0,0,1,0,0,1,1,0,0,1,1,0,0,1,1,0     ; Scanlines
 NEWSYM GUIINVID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0     ; Interpolation
 NEWSYM GUIEAVID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0     ; Eagle
 NEWSYM GUIIEVID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0     ; (Interp | Eagle)
-NEWSYM GUIFSVID, db 0,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0     ; Full Screen
-NEWSYM GUISSVID, db 0,0,0,0,0,0,1,0,0,1,1,0,0,1,1,0     ; Small Screen
+NEWSYM GUIFSVID, db 0,0,0,0,0,0,0,1,1,1,1,0,0,1,1,0     ; Full Screen
+NEWSYM GUIWSVID, db 0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0     ; (Full Scr. | Wide Scr.)
+NEWSYM GUISSVID, db 0,0,0,0,0,0,1,0,0,0,0,0,0,1,1,0     ; Small Screen
 NEWSYM GUITBVID, db 0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0     ; Triple Buffering
 NEWSYM GUIHSVID, db 0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0     ; Half/Quarter Scanlines
 NEWSYM GUI2xVID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0     ; 2xSaI/Super Eagle Engines
 NEWSYM GUIWFVID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0     ; If Windows Full Screen
-NEWSYM GUII2VID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0     ; Interpolation
-NEWSYM GUIM7VID, db 0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0     ; Interpolation
+NEWSYM GUII2VID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0     ; Super 2xSAI Engine
+NEWSYM GUIM7VID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0     ; Hi-Resolution Mode 7
 NEWSYM GUIBIFIL, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0     ; Bilinear Filtering
-NEWSYM GUITBWVID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0     ; Triple Buffering (Win)
+NEWSYM GUITBWVID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0    ; Triple Buffering (Win)
 SECTION .text
 
 ; ****************************
