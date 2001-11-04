c71673df98399a729973aaaac328e55e47153e5c
stainless <stainless@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2001-11-04 05:27:31 +0000

    Added full screen to 320x480 video modes in the DOS port.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@1080 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/dos/dosintrf.asm
+++ src/dos/dosintrf.asm
@@ -1047,7 +1047,6 @@ NEWSYM GUIINVID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0     ; Interpolation
 NEWSYM GUIEAVID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0     ; Eagle
 NEWSYM GUIIEVID, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0     ; (Interp | Eagle)
 NEWSYM GUIFSVID, db 0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,1,1,0     ; Full Screen
-NEWSYM GUIWSVID, db 0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0     ; (Full Scr. | Wide Scr.)
 NEWSYM GUISSVID, db 0,0,0,0,0,0,1,0,0,0,0,1,1,1,1,1,1,0     ; Small Screen
 NEWSYM GUITBVID, db 0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0     ; Triple Buffering
 NEWSYM GUIHSVID, db 0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0     ; Half/Quarter Scanlines
