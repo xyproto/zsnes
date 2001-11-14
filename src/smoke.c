89102335ea14ad6c5349d90c9512ca6a2f74217e
stainless <stainless@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2001-11-14 09:55:15 +0000

    Optimized and cleaned-up 'smoke.c' (smoke effect).
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@1104 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/smoke.c
+++ src/smoke.c
@@ -5,20 +5,27 @@
 #else
 #   include <stdlib.h>
 #   include <string.h>
+#   include <time.h>
 #endif
 
 
-// Ripped from an Allegro example (exflame.c). :P
-// Should be fire, but looks more like smoke in ZSNES.
+/*
 
-#define MIN(x,y)    \
+Ripped from an Allegro example (exflame.c). :)
+
+Should be fire, but looks more like smoke in ZSNES.
+
+*/
+
+
+#define MIN(x, y)    \
     (((x) < (y)) ? (x) : (y))
 
 #define ABS(x)      \
     (((x) >= 0) ? (x) : ( - (x)))
 
 
-#define FIRE_HOTSPOTS 80
+#define FIRE_HOTSPOTS   80
 
 static int fire_hotspot [FIRE_HOTSPOTS];
 
@@ -26,9 +33,9 @@ static int fire_hotspot [FIRE_HOTSPOTS];
 extern char * vidbuffer;
 
 
-#define SCRW 288
+#define SCRW    288
 
-#define SCRH 224
+#define SCRH    224
 
 
 static unsigned char fire_line [SCRW];
@@ -39,11 +46,13 @@ static unsigned char fire_buffer [SCRW * SCRH];
 static int fire_init_flag;
 
 
-static void draw_bottom_line_of_fire (void)
+static __inline__ void draw_bottom_line_of_fire (void)
 {
-   int count, count2;
+    int count, count2;
 
-    memset (&fire_line, 0, SCRW);
+
+    memset ((& fire_line), 0, SCRW);
+
 
     for (count = 0; count < FIRE_HOTSPOTS; count ++)
     {
@@ -53,12 +62,14 @@ static void draw_bottom_line_of_fire (void)
             if ((count2 >= 0) && (count2 < SCRW))
             {
                 fire_line [count2] =
-                    MIN((fire_line [count2] + 20) -
-                        ABS(fire_hotspot [count] - count2), 256);
+                    MIN ((fire_line [count2] + 20) -
+                    ABS (fire_hotspot [count] - count2), 256);
             }
         }
 
+
         fire_hotspot [count] += ((rand () & 7) - 3);
+
 
         if (fire_hotspot [count] < 0)
         {
@@ -70,26 +81,33 @@ static void draw_bottom_line_of_fire (void)
         }
     }
 
+
     for (count = 0; count < SCRW; count ++)
     {
         fire_buffer [((SCRH - 1) *
-                      (SCRW)) + count] = fire_line [count];
+            (SCRW)) + count] = fire_line [count];
     }
 }
 
 
-static void InitFire (void)
+static __inline__ void init_fire (void)
 {
     int x, y, pixel, count;
+
+
+    srand (time (0));
+
 
     for (count = 0; count < FIRE_HOTSPOTS; count ++)
     {
         fire_hotspot [count] = (rand () % SCRW);
     }
 
+
     for (count = 0; count < SCRH; count ++)
     {
         draw_bottom_line_of_fire ();
+
 
         for (y = 0; y < (SCRH - 1); y ++)
         {
@@ -97,31 +115,38 @@ static void InitFire (void)
             {
                 pixel = fire_buffer [((y + 1) * SCRW) + x];
     
+
                 if (pixel > 0)
                 {
                     pixel --;
                 }
-    
+
+
                 fire_buffer [(y * SCRW) + x] = pixel;
             }
         }
     }
 
+
     fire_init_flag = 1;
 }
 
 
-// void DrawFire (void)
+/* void DrawFire (void) */
+
 void DrawSmoke (void)
 {
     int x, y, pixel, pixel2;
 
-    if (fire_init_flag != 1)
+
+    if (! fire_init_flag)
     {
-        InitFire ();
+        init_fire ();
     }
 
+
     draw_bottom_line_of_fire ();
+
 
     for (y = 0; y < (SCRH - 1); y ++)
     {
@@ -129,14 +154,17 @@ void DrawSmoke (void)
         {
             pixel = fire_buffer [((y + 1) * SCRW) + x];
 
+
             if (pixel > 0)
             {
                 pixel --;
             }
 
+
             fire_buffer [(y * SCRW) + x] = pixel;
         }
     }
+
 
     for (y = 0; y < SCRH; y ++)
     {
@@ -145,6 +173,7 @@ void DrawSmoke (void)
             pixel = vidbuffer [(y * SCRW) + x];
 
             pixel2 = (fire_buffer [(y * SCRW) + x] / 8);
+
 
             if (pixel2 > pixel)
             {
