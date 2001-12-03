89a3514d45eb275bc46bb388c97b73ff5ece7ea2
pagefault <pagefault@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2001-12-03 20:49:25 +0000

    Removed unneeded hack
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@1123 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/init.asm
+++ src/init.asm
@@ -1642,20 +1642,6 @@ NEWSYM headerhack
     mov byte[opexec358cph],58
 .notequinox
 
-    mov esi,[romdata]
-    add esi,07FC0h
-    cmp dword[esi],'DRAG'
-    jne .notdq5
-    cmp dword[esi+4],'ONQU'
-    jne .notdq5
-    cmp dword[esi+8],'EST5'
-    jne .notdq5
-    mov byte[opexec268],220
-    mov byte[opexec358],220
-    mov byte[opexec268cph],64
-    mov byte[opexec358cph],64
-.notdq5
-
     ; Lamborgini Challenge - -p 110
     mov esi,[romdata]
     add esi,07FC0h
