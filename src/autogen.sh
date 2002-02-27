1bc2987e373c9bef5a68d83195d367f10916e50b
theoddone33 <theoddone33@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2002-02-27 05:21:00 +0000

    Move 'fixes', configure run automatically and timestamp on ./configure.in not updated.  This is what I get for stealing someone else's autogen.sh
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@1151 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/autogen.sh
+++ src/autogen.sh
@@ -2,15 +2,9 @@
 
 echo "Generating build information using aclocal and autoconf..."
 
-# Touch the timestamps on all the files since CVS messes them up
-directory=`dirname $0`
-touch $directory/configure.in
-
 # Regenerate configuration files
 aclocal
 autoconf
 
 # Run configure for this platform
-#./configure $*
-echo "Now you are ready to run ./configure"
-
+./configure $*
