8cde494b833db338b3a8798c8b2ddd33cb84b357
theoddone33 <theoddone33@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2002-02-27 04:44:28 +0000

    configure and aclocal.m4 depricated by autogen.sh, everyone should run that if they're playing with CVS.
    changed echos in the configure script to comments
    
    
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@1146 8cdf4848-9e13-0410-98e6-c9478ff1fb58

---
+++ src/autogen.sh
@@ -0,0 +1,17 @@
+#!/bin/ah
+
+echo "Generating build information using aclocal, automake and autoconf"
+echo "This may take a while ..."
+
+# Touch the timestamps on all the files since CVS messes them up
+directory=`dirname $0`
+touch $directory/configure.in
+
+# Regenerate configuration files
+aclocal
+autoconf
+
+# Run configure for this platform
+#./configure $*
+echo "Now you are ready to run ./configure"
+
