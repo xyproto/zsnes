36e18ad325755396e15c51b49203a2fe00e4d036
theoddone33 <theoddone33@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2002-07-17 04:17:35 +0000

    Fix problem with SDL include, thanks asfand
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@1294 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/autogen.sh
+++ src/autogen.sh
@@ -3,7 +3,7 @@
 echo "Generating build information using aclocal and autoconf..."
 
 # Regenerate configuration files
-aclocal
+aclocal	`sdl-config --prefix`/share/aclocal	# thanks asfand
 autoconf
 
 # Run configure for this platform
