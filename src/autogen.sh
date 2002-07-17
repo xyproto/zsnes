c1c0b08169ef10c4716c6df9f4bf64d943668b01
theoddone33 <theoddone33@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2002-07-17 04:19:29 +0000

    Fix the fix
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@1295 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/autogen.sh
+++ src/autogen.sh
@@ -3,7 +3,7 @@
 echo "Generating build information using aclocal and autoconf..."
 
 # Regenerate configuration files
-aclocal	`sdl-config --prefix`/share/aclocal	# thanks asfand
+aclocal	--acdir=`sdl-config --prefix`/share/aclocal	# thanks asfand
 autoconf
 
 # Run configure for this platform
