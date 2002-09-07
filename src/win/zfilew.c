d82fa7123453b5d4fa91391a00b6e4f1c5514dde
z_demo_z <z_demo_z@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2002-09-07 23:55:40 +0000

    CC test version
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@1305 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/win/zfilew.c
+++ src/win/zfilew.c
@@ -16,7 +16,7 @@
 //Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 // Enable use of network loaded games with zsnes:// file names
-#define CCBETA
+//#define CCBETA
 
 // Enable debug of network loaded games
 //#define CCDEBUG
