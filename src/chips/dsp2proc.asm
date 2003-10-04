501bf6eb6c490f5ca42c0ab50ba706d86f43e170
pagefault <pagefault@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2003-10-04 22:17:07 +0000

    DSP-2 code, this a placeholder only at the moment
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@1576 8cdf4848-9e13-0410-98e6-c9478ff1fb58

---
+++ src/chips/dsp2proc.asm
@@ -0,0 +1,24 @@
+;Copyright (C) 1997-2001 ZSNES Team ( zsknight@zsnes.com / _demo_@zsnes.com )
+;
+;This program is free software; you can redistribute it and/or
+;modify it under the terms of the GNU General Public License
+;as published by the Free Software Foundation; either
+;version 2 of the License, or (at your option) any later
+;version.
+;
+;This program is distributed in the hope that it will be useful,
+;but WITHOUT ANY WARRANTY; without even the implied warranty of
+;MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;GNU General Public License for more details.
+;
+;You should have received a copy of the GNU General Public License
+;along with this program; if not, write to the Free Software
+;Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+%include "macros.mac"
+
+EXTSYM regaccessbankr8,regaccessbankr16,regaccessbankw8,regaccessbankw16
+
+NEWSYM Dsp2ProcAsmStart
+
+NEWSYM Dsp2ProcAsmEnd
