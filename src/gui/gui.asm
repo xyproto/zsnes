afb0fa8c71bcfa893aebfdd3c8ee57f6c66e8fcc
pagefault <pagefault@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2003-11-03 06:57:44 +0000

    Expand GUI video dialog
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@1625 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/gui/gui.asm
+++ src/gui/gui.asm
@@ -634,7 +634,7 @@ SECTION .data
 ;                LOAD STAT INPT OPT  VID  SND  CHT  NET  GMKEY GUIOP ABT  RSET SRC  STCN MOVE CMBO ADDO
 GUIwinposxo dd 0,5   ,60  ,30  ,55  ,50  ,35  ,5   ,30  ,10   ,10   ,50  ,65  ,20  ,70  ,50  ,3   ,50
 GUIwinposyo dd 0,20  ,70  ,30  ,20  ,20  ,20  ,20  ,30  ,20   ,20   ,20  ,60  ,30  ,65  ,50  ,22  ,60
-GUIwinsizex dd 0,244 ,126 ,189 ,167 ,170 ,188 ,244 ,8*16,235  ,240  ,190 ,9*16,8*16,9*16,140 ,250 ,160
+GUIwinsizex dd 0,244 ,126 ,189 ,167 ,180 ,188 ,244 ,8*16,235  ,240  ,190 ,9*16,8*16,9*16,140 ,250 ,160
 GUIwinsizey dd 0,190 ,3*16,166 ,190 ,192 ,188 ,191 ,40  ,189  ,150  ,190 ,42  ,40  ,42  ,70  ,190 ,100
 GUIwinptr   db 0
 
