e261b4363d0cc299d9f551d72d997a5fa850f8b5
grinvader <grinvader@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2005-01-19 19:35:02 +0000

    Copyright fix, sorry. -_-;
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@2003 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/cpu/executec.c
+++ src/cpu/executec.c
@@ -1,10 +1,8 @@
 /*
 Copyright (C) 1997-2005 ZSNES Team ( zsKnight, _Demo_, pagefault, Nach )
 
-zsknight@zsnes.com
-_demo_@zsnes.com
-pagefault@zsnes.com
-n-a-c-h@users.sf.net
+http://www.zsnes.com
+http://sourceforge.net/projects/zsnes
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -21,6 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
+
 
 
 
