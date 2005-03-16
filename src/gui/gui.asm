d3b309eac86ef7f159e52e5f3785ad6338edfc44
ipher <ipher@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2005-03-16 16:17:09 +0000

    Prevent total destruction of cfg files
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@2230 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/gui/gui.asm
+++ src/gui/gui.asm
@@ -380,9 +380,6 @@ NEWSYM KeyStateSlc6,  dd 0
 NEWSYM KeyStateSlc7,  dd 0
 NEWSYM KeyStateSlc8,  dd 0
 NEWSYM KeyStateSlc9,  dd 0
-NEWSYM KeyInsrtChap,  dd 0
-NEWSYM KeyNextChap,   dd 0
-NEWSYM KeyPrevChap,   dd 0
 
 GUIshowallext db 0
 GUIloadfntype    db 0
@@ -650,6 +647,9 @@ NEWSYM SRAMState, db 0
 NEWSYM RewindStates, db 16
 NEWSYM PrimaryBuffer, db 0
 NEWSYM RewindFrames, db 6
+NEWSYM KeyInsrtChap,  dd 0
+NEWSYM KeyNextChap,   dd 0
+NEWSYM KeyPrevChap,   dd 0
 
 GUIsave equ $-GUIRAdd
 
@@ -679,9 +679,9 @@ ModemOKStat  resb 1       ; OK is detected on modem status
 SECTION .data             ; Window sizes and positions
 ;                LOAD STAT INPT OPT  VID  SND  CHT  NET  GMKEY GUIOP ABT  RSET SRC  STCN MOVE CMBO ADDO CHIP PATH SAVE
 GUIwinposxo dd 0,5   ,60  ,30  ,55  ,50  ,35  ,5   ,30  ,10   ,10   ,50  ,65  ,20  ,70  ,50  ,3   ,50  ,50  ,5    ,20
-GUIwinposyo dd 0,20  ,70  ,30  ,20  ,20  ,20  ,20  ,30  ,20   ,20   ,20  ,60  ,30  ,65  ,50  ,22  ,60  ,60  ,20   ,60
+GUIwinposyo dd 0,20  ,70  ,30  ,20  ,20  ,20  ,20  ,30  ,20   ,20   ,20  ,60  ,30  ,65  ,40  ,22  ,60  ,60  ,20   ,60
 GUIwinsizex dd 0,244 ,126 ,189 ,167 ,180 ,188 ,244 ,8*16,235  ,240  ,190 ,9*16,8*16,9*16,140 ,250 ,160 ,160 ,244  ,200
-GUIwinsizey dd 0,190 ,3*16,166 ,190 ,192 ,188 ,191 ,40  ,189  ,150  ,190 ,42  ,40  ,42  ,120  ,190 ,100 ,100 ,190  ,150
+GUIwinsizey dd 0,190 ,3*16,166 ,190 ,192 ,188 ,191 ,40  ,189  ,150  ,190 ,42  ,40  ,42  ,160  ,190 ,100 ,100 ,190 ,150
 GUIwinptr   db 0
 
 section .bss
