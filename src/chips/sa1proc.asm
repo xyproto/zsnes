0a742d2355adcb0639500f18c88b168ed7ca8614
pagefault <pagefault@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2005-03-29 00:51:21 +0000

    Fixed SA-1 IRQ timing
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@2303 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/chips/sa1proc.asm
+++ src/chips/sa1proc.asm
@@ -121,7 +121,7 @@ NEWSYM SA1Swap
     ; Check if IRQ is executed on SA-1
     xor eax,eax
     mov al,dl
-    add dh,100
+    add dh,35
     mov edi,[SA1tablead+eax*4]
     mov byte[SA1Status],1
     test dword[SA1DoIRQ],0FF000003h
