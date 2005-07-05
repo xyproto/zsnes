74a39be3a310dcb8dfa1b453c6f4753245d202a5
pagefault <pagefault@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2005-07-05 21:17:38 +0000

    Added file to contain new timing code
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@2563 8cdf4848-9e13-0410-98e6-c9478ff1fb58

---
+++ src/cpu/execute.c
@@ -0,0 +1,35 @@
+/*
+Copyright (C) 1997-2005 ZSNES Team ( zsKnight, _Demo_, pagefault, Nach )
+
+http://www.zsnes.com
+http://sourceforge.net/projects/zsnes
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+
+#ifdef __LINUX__
+#include "../gblhdr.h"
+#define DIR_SLASH "/"
+#else
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#define DIR_SLASH "\\"
+#endif
+#include "../gblvars.h"
