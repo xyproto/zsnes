b1331254df2e341f872001ca6c3036a83d86f112
n-a-c-h <n-a-c-h@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2006-02-14 03:31:13 +0000

    Fixed URL (pagefault, please put something other than phpinfo() there). Fixed a warning. Todo: improve WIP version check.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@2786 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/win/winlink.cpp
+++ src/win/winlink.cpp
@@ -3151,11 +3151,11 @@ void DisplayWIPDisclaimer()
 
    strcpy(BuildWorkBuffer, __DATE__);
 
-   for (int i = 0; i<strlen(BuildWorkBuffer); i++) CurrentBuildNum += BuildWorkBuffer[i];
+   for (unsigned int i = 0; i<strlen(BuildWorkBuffer); i++) CurrentBuildNum += BuildWorkBuffer[i];
 
    if (CurrentBuildNum != PrevBuildNum)
    {
-      MessageBox(NULL, "This build of ZSNES is a WORK IN PROGRESS. This means that it is known to contain bugs and certain features\nmay or may not be working correctly. This build is not any representation of final work and is provided AS IS\nfor people to try bleeding edge code.\n\nPlease see http://zsnes.gamehost.com/~pagefault/ for a list of current issues.", "Disclaimer", MB_OK);
+      MessageBox(NULL, "This build of ZSNES is a WORK IN PROGRESS. This means that it is known to contain bugs and certain features\nmay or may not be working correctly. This build is not any representation of final work and is provided AS IS\nfor people to try bleeding edge code.\n\nPlease see http://zsnes.game-host.org/~pagefault/ for a list of current issues.", "Disclaimer", MB_OK);
       PrevBuildNum = CurrentBuildNum;
    }
 
