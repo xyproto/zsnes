eba1ddbc30672d73fec4f77cba9f0f8693f71585
grinvader <grinvader@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2006-03-01 01:54:33 +0000

    Unused extsym.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@2866 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/cpu/dma.asm
+++ src/cpu/dma.asm
@@ -22,7 +22,7 @@
 
 EXTSYM memtabler8,regptw,snesmap2,snesmmap,memtablew8,regptr,memtabler16
 EXTSYM dmadata,hdmatype,nexthdma,resolutn,curhdma,curypos,disablehdma,hdmadata
-EXTSYM hdmadelay,hdmarestart,nohdmaframe,SPCRAM
+EXTSYM hdmadelay,hdmarestart,nohdmaframe
 
 ;*******************************************************
 ; Transfer DMA                     Inits & Transfers DMA
