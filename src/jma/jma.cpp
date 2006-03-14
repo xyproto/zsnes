940ce85acab6d826050ea58c5b46e3218b7a6fe4
n-a-c-h <n-a-c-h@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2006-03-14 03:26:36 +0000

    We agree with Linus, we don't need someone changing the rules on us.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@2928 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/jma/jma.cpp
+++ src/jma/jma.cpp
@@ -3,9 +3,7 @@ Copyright (C) 2005-2006 NSRT Team ( http://nsrt.edgeemu.com )
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later
-version.
+version 2 as published by the Free Software Foundation.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
