c2090f096f5291ebf2dd190354cbc509236fd8a9
Nach <Nach@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2006-06-19 13:19:33 +0000

    New scancode handling system, should fix problems with non US keyboard and Mac OS X. Thanks hector.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@3301 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/linux/sdllink.c
+++ src/linux/sdllink.c
@@ -113,7 +113,6 @@ static float MouseMaxX = 256;
 static float MouseMinY = 0;
 static float MouseMaxY = 223;
 static int MouseX, MouseY;
-static float MouseMoveX, MouseMoveY;
 static int MouseMove2X, MouseMove2Y;
 unsigned char MouseButton;
 static float MouseXScale = 1.0;
@@ -155,7 +154,8 @@ extern unsigned char romispal;
 void clearwin (void);
 void drawscreenwin(void);
 void initwinvideo();
-void ProcessKeyBuf(int scancode);
+unsigned int sdl_keysym_to_pc_scancode(int);
+void ProcessKeyBuf(int);
 void LinuxExit(void);
 void UpdateSound(void *userdata, Uint8 * stream, int len);
 
@@ -182,6 +182,7 @@ static void adjustMouseYScale(void)
 int Main_Proc(void)
 {
   SDL_Event event;
+  unsigned int key;
 
   while (SDL_PollEvent(&event))
   {
@@ -203,10 +204,11 @@ int Main_Proc(void)
           numlockptr = 1;
         else
           numlockptr = 0;
-        if (event.key.keysym.scancode - 8 >= 0)
+
+        key = sdl_keysym_to_pc_scancode(event.key.keysym.sym);
+        if (key < 448)
         {
-          //if (pressed[event.key.keysym.scancode - 8] != 2)
-          pressed[event.key.keysym.scancode - 8] = 1;
+          pressed[key] = 1;
           ProcessKeyBuf(event.key.keysym.sym);
         }
         break;
@@ -215,8 +217,11 @@ int Main_Proc(void)
         if (event.key.keysym.sym == SDLK_LSHIFT ||
             event.key.keysym.sym == SDLK_RSHIFT)
           shiftptr = 0;
-        if (event.key.keysym.scancode - 8 >= 0)
-          pressed[event.key.keysym.scancode - 8] = 0;
+        key = sdl_keysym_to_pc_scancode(event.key.keysym.sym);
+        if (key < 448)
+        {
+          pressed[key] = 0;
+        }
         break;
 
       case SDL_MOUSEMOTION:
@@ -452,6 +457,110 @@ int Main_Proc(void)
   return TRUE;
 }
 
+unsigned int sdl_keysym_to_pc_scancode(int sym)
+{
+  switch (sym)
+  {
+    case SDLK_ESCAPE:       return 0x01;
+    case SDLK_1:            return 0x02;
+    case SDLK_2:            return 0x03;
+    case SDLK_3:            return 0x04;
+    case SDLK_4:            return 0x05;
+    case SDLK_5:            return 0x06;
+    case SDLK_6:            return 0x07;
+    case SDLK_7:            return 0x08;
+    case SDLK_8:            return 0x09;
+    case SDLK_9:            return 0x0a;
+    case SDLK_0:            return 0x0b;
+    case SDLK_MINUS:        return 0x0c;
+    case SDLK_EQUALS:       return 0x0d;
+    case SDLK_BACKSPACE:    return 0x0e;
+    case SDLK_TAB:          return 0x0f;
+    case SDLK_q:            return 0x10;
+    case SDLK_w:            return 0x11;
+    case SDLK_e:            return 0x12;
+    case SDLK_r:            return 0x13;
+    case SDLK_t:            return 0x14;
+    case SDLK_y:            return 0x15;
+    case SDLK_u:            return 0x16;
+    case SDLK_i:            return 0x17;
+    case SDLK_o:            return 0x18;
+    case SDLK_p:            return 0x19;
+    case SDLK_LEFTBRACKET:  return 0x1a;
+    case SDLK_RIGHTBRACKET: return 0x1b;
+    case SDLK_RETURN:       return 0x1c;
+    case SDLK_LCTRL:        return 0x1d;
+    case SDLK_a:            return 0x1e;
+    case SDLK_s:            return 0x1f;
+    case SDLK_d:            return 0x20;
+    case SDLK_f:            return 0x21;
+    case SDLK_g:            return 0x22;
+    case SDLK_h:            return 0x23;
+    case SDLK_j:            return 0x24;
+    case SDLK_k:            return 0x25;
+    case SDLK_l:            return 0x26;
+    case SDLK_SEMICOLON:    return 0x27;
+    case SDLK_QUOTE:        return 0x28;
+    case SDLK_BACKQUOTE:
+    case SDLK_HASH:         return 0x29;
+    case SDLK_LSHIFT:       return 0x2a;
+    case SDLK_BACKSLASH:    return 0x2b;
+    case SDLK_z:            return 0x2c;
+    case SDLK_x:            return 0x2d;
+    case SDLK_c:            return 0x2e;
+    case SDLK_v:            return 0x2f;
+    case SDLK_b:            return 0x30;
+    case SDLK_n:            return 0x31;
+    case SDLK_m:            return 0x32;
+    case SDLK_COMMA:        return 0x33;
+    case SDLK_PERIOD:       return 0x34;
+    case SDLK_SLASH:        return 0x35;
+    case SDLK_RSHIFT:       return 0x36;
+    case SDLK_KP_MULTIPLY:  return 0x37;
+    case SDLK_LALT:         return 0x38;
+    case SDLK_SPACE:        return 0x39;
+    case SDLK_CAPSLOCK:     return 0x3a;
+    case SDLK_F1:           return 0x3b;
+    case SDLK_F2:           return 0x3c;
+    case SDLK_F3:           return 0x3d;
+    case SDLK_F4:           return 0x3e;
+    case SDLK_F5:           return 0x3f;
+    case SDLK_F6:           return 0x40;
+    case SDLK_F7:           return 0x41;
+    case SDLK_F8:           return 0x42;
+    case SDLK_F9:           return 0x43;
+    case SDLK_F10:          return 0x44;
+    case SDLK_NUMLOCK:      return 0x45;
+    case SDLK_SCROLLOCK:    return 0x46;
+    case SDLK_KP7:          return 0x47;
+    case SDLK_KP8:          return 0x48;
+    case SDLK_KP9:          return 0x49;
+    case SDLK_KP_MINUS:     return 0x4a;
+    case SDLK_KP4:          return 0x4b;
+    case SDLK_KP5:          return 0x4c;
+    case SDLK_KP6:          return 0x4d;
+    case SDLK_KP_PLUS:      return 0x4e;
+    case SDLK_KP1:          return 0x4f;
+    case SDLK_KP2:          return 0x50;
+    case SDLK_KP3:          return 0x51;
+    case SDLK_KP0:          return 0x52;
+    case SDLK_KP_PERIOD:    return 0x53;
+    case SDLK_F11:          return 0x57;
+    case SDLK_F12:          return 0x58;
+    case SDLK_HOME:         return 0x59;
+    case SDLK_UP:           return 0x5a;
+    case SDLK_PAGEUP:       return 0x5b;
+    case SDLK_LEFT:         return 0x5c;
+    case SDLK_RIGHT:        return 0x5e;
+    case SDLK_END:          return 0x5f;
+    case SDLK_DOWN:         return 0x60;
+    case SDLK_PAGEDOWN:     return 0x61;
+    case SDLK_INSERT:       return 0x62;
+    case SDLK_DELETE:       return 0x63;
+
+  }
+  return(0x64+sym);
+}
 
 void ProcessKeyBuf(int scancode)
 {
