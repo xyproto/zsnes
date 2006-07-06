093740b7a243db36c661dd2da08a8cd6b7490824
grinvader <grinvader@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2006-07-06 16:53:48 +0000

    Some new rules, now modifying Makefile.in/configure.in won't require a manual autogen.sh run - make will work it out. Some name changes along the way.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@3499 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/autogen.sh
+++ src/autogen.sh
@@ -3,8 +3,13 @@
 echo "Generating build information using aclocal and autoconf..."
 
 # Regenerate configuration files
-aclocal	--acdir=`sdl-config --prefix`/share/aclocal	# thanks asfand
+aclocal  --acdir=`sdl-config --prefix`/share/aclocal	# thanks asfand
 autoconf
 
-# Run configure for this platform
-./configure $*
+# Run configure for this platform, or simply update Makefile
+case $1 in
+  --recheck )
+    ./config.status --recheck; break ;;
+  * )
+    ./configure $*; break ;;
+esac
