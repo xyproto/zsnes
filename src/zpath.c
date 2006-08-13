4d0e50f0a98001138131006c7bd88a54d7957627
Nach <Nach@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2006-08-13 16:12:00 +0000

    Added missing include.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@3687 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/zpath.c
+++ src/zpath.c
@@ -29,6 +29,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #include "win/safelib.h"
 #include "win/lib.h"
 #else
+#include "dos/lib.h"
 #include <unistd.h>
 #endif
 #include <string.h>
