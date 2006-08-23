06349f94b01a3b4f99e2045f16ac9599952b0aa7
pagefault <pagefault@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2006-08-23 19:39:58 +0000

    Adjust modes for NTSC filter
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@3706 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/linux/sdlintrf.asm
+++ src/linux/sdlintrf.asm
@@ -592,7 +592,7 @@ NEWSYM GUIHQ2X,   db 0,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1 ; hq2x filt
 NEWSYM GUIHQ3X,   db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 ; (hq3x filter)
 NEWSYM GUIHQ4X,   db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 ; (hq4x filter)
 NEWSYM GUIRESIZE, db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0 ; SDL Resizable
-NEWSYM GUINTVID,  db 0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 ; NTSC Filter
+NEWSYM GUINTVID,  db 0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 ; NTSC Filter
 
 SECTION .text
 
