fd5a1ffddd6cfaf29a12cbe48e118946c938a832
Deathlike2 <Deathlike2@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2006-09-21 01:48:20 +0000

    Fixed DOS compiler warnings.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@3871 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/gui/guifuncs.c
+++ src/gui/guifuncs.c
@@ -1401,7 +1401,7 @@ void GUILoadData()
         dupfound = (!fnamencmp(nameptr, (char *)prevloadfnamel+i*512, 512) && (!fnamencmp(ZRomPath, (char *)prevloaddnamel+i*512+1, 512)));
         if(dupfound && modheader)
         {
-          strncpy(prevloadiname+i*28, selected_names[GUIcurrentcursloc], 28);
+          strncpy((char *)prevloadiname+i*28, selected_names[GUIcurrentcursloc], 28);
           prevloadiname[i*28+27] = 0;
           modheader = false;
         }
@@ -1411,7 +1411,7 @@ void GUILoadData()
 
       if (!dupfound)
       {
-        strncpy(prevloadiname+9*28, selected_names[GUIcurrentcursloc], 28);
+        strncpy((char *)prevloadiname+9*28, selected_names[GUIcurrentcursloc], 28);
         prevloadiname[9*28+27] = 0;
         strcpy((char *)prevloaddnamel+9*512+1, ZRomPath);
         strcpy((char *)prevloadfnamel+9*512, ZCartName);
