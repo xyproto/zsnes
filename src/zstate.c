ec3835a63b24ac4265eaef4363d713fe01ee97a3
Nach <Nach@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2006-09-27 23:19:40 +0000

    Added missing include.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@3916 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/zstate.c
+++ src/zstate.c
@@ -27,6 +27,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <zlib.h>
 #include <time.h>
 #ifdef __WIN32__
