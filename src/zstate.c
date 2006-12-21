d6e7626080717d061482a3edfef42ee2b7c6b5b0
jbo_85 <jbo_85@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2006-12-21 02:37:23 +0000

    Removed hack to get the right internal name for EHi games in SPC files.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@4125 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/zstate.c
+++ src/zstate.c
@@ -1381,7 +1381,7 @@ void savespcdata()
 
       memset(ssdatst+0x2E, 0, 32); //0002Eh-0004Dh - SubTitle/Song Name
       memset(ssdatst+0x4E, 0, 32); //0004Eh-0006Dh - Title of Game
-      memcpy(ssdatst+0x4E, ((unsigned char *)romdata)+infoloc-((infoloc == 0x40ffc0) ? 0x408000 : 0), 21);
+      memcpy(ssdatst+0x4E, ((unsigned char *)romdata)+infoloc, 21);
       memset(ssdatst+0x6E, 0, 16); //0006Eh-0007Dh - Name of Dumper
       memset(ssdatst+0x7E, 0, 32); //0007Eh-0009Dh - Comments
 
