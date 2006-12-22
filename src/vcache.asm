03108463712d972e6877633efe81469b6edc3fc7
Deathlike2 <Deathlike2@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2006-12-22 00:51:18 +0000

    Removed useless EXTSYM that is causing MSVC not to compile happily.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@4135 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/vcache.asm
+++ src/vcache.asm
@@ -23,7 +23,7 @@
 EXTSYM MessageOn,MsgCount,Msgptr,Voice0Disable,Voice0Status,Voice1Disable
 EXTSYM Voice1Status,Voice2Disable,Voice2Status,Voice3Disable,Voice3Status
 EXTSYM Voice4Disable,Voice4Status,Voice5Disable,Voice5Status,Voice6Disable
-EXTSYM Voice6Status,Voice7Disable,Voice7Status,bgcmsung,bgmode,cbackofsaddr
+EXTSYM Voice6Status,Voice7Disable,Voice7Status,bgcmsung,bgmode
 EXTSYM cgmod,disableeffects,frameskip,frskipper,current_zst,zst_name
 EXTSYM maxbr,modeused,mousexloc,mouseyloc,newengen
 EXTSYM nextdrawallng,pal16b,pal16bxcl,pressed,prevbright,prevpal
