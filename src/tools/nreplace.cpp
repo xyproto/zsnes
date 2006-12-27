20074e91d6dfb6e5a1e43ae689c926ba1f6c1c27
Nach <Nach@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2006-12-27 11:04:05 +0000

    Yeah copyright.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@4197 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/tools/nreplace.cpp
+++ src/tools/nreplace.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2005-2006 Nach, grinvader ( http://www.zsnes.com )
+Copyright (C) 2005-2007 Nach, grinvader ( http://www.zsnes.com )
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
