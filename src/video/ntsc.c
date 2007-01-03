848dd8102c8f2345d7dc8f15be8410b74fd2f4bc
Nach <Nach@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2007-01-03 01:27:50 +0000

    License Update and formatting.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@4224 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/video/ntsc.c
+++ src/video/ntsc.c
@@ -1,24 +1,36 @@
+/*
+Copyright (C) 2006-2007 Shay Green.
 
-/* snes_ntsc 0.2.1. http://www.slack.net/~ant/ */
+http://www.slack.net/~ant/
 
-/* compilable in C or C++; just change the file extension */
+This module is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2.1
+of the License, or (at your option) any later version.
+
+This module is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this module; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+
+/*
+SNES NTSC composite video to RGB emulator/blitter
+snes_ntsc 0.2.1. http://www.slack.net/~ant/
+Based on algorithm by NewRisingSun
+*/
+
 
 #include "ntsc.h"
 
 #include <assert.h>
 #include <math.h>
 
-/* Based on algorithm by NewRisingSun */
-/* Copyright (C) 2006 Shay Green. This module is free software; you
-can redistribute it and/or modify it under the terms of the GNU Lesser
-General Public License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version. This
-module is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-more details. You should have received a copy of the GNU Lesser General
-Public License along with this module; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA */
 
 enum { disable_correction = 0 }; /* for debugging */
 
--- src/video/ntsc.h
+++ src/video/ntsc.h
@@ -1,7 +1,30 @@
+/*
+Copyright (C) 2006-2007 Shay Green.
 
-/* SNES NTSC composite video to RGB emulator/blitter */
+http://www.slack.net/~ant/
 
-/* snes_ntsc 0.2.1 */
+This module is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2.1
+of the License, or (at your option) any later version.
+
+This module is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this module; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+
+/*
+SNES NTSC composite video to RGB emulator/blitter
+snes_ntsc 0.2.1. http://www.slack.net/~ant/
+Based on algorithm by NewRisingSun
+*/
+
 
 #ifndef SNES_NTSC_H
 #define SNES_NTSC_H
