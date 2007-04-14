e9004db6b1aa2a397e8f518b413220e74e910699
Nach <Nach@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2007-04-14 23:23:53 +0000

    Fixing stupidity.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@4941 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/gblvars.h
+++ src/gblvars.h
@@ -28,11 +28,12 @@ extern uint32_t PHnum2writesa1reg, SA1Mode, prevedi, SA1xpc, sa1dmaptr;
 extern uint32_t soundcycleft, spc700read, timer2upd, xa, PHnum2writesfxreg;
 extern uint32_t opcd, HIRQCycNext, oamaddr, curexecstate, nmiprevaddrl;
 extern uint32_t nmirept, nmiprevline, nmistatus, joycontren;
-extern uint32_t SfxR0, *setaramdata, ramsize, nmiprevaddrh;
+extern uint32_t SfxR0, ramsize, nmiprevaddrh;
 extern uint32_t tempesi, tempedi, tempedx, tempebp;
 extern uint32_t SPCMultA, PHnum2writespc7110reg, PHdspsave2;
 
-extern uintptr_t *wramdata, *vram, *C4Ram, *romdata, *setaramdata, *sram, *sfxramdata, *SA1RAMArea;
+extern uint32_t *wramdata, *vram, *C4Ram, *sfxramdata, *setaramdata, *sram;
+extern uint8_t *romdata, *SA1RAMArea;
 
 extern unsigned char sndrot, SPCRAM[65472], DSPMem[256], SA1Status;
 extern unsigned char DSP1Enable, DSP1COp, prevoamptr, BRRBuffer[32];
