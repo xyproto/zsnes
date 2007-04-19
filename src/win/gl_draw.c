5224442a9933ec6dd5bca0705fe22e336a7fbe4b
Nach <Nach@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2007-04-19 03:01:43 +0000

    Fixed a #include
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@4965 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/win/gl_draw.c
+++ src/win/gl_draw.c
@@ -25,7 +25,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #include <GL/glext.h>
 #include "gl_draw.h"
 #include "../cfg.h"
-#include "win/winlink.h"
+#include "winlink.h"
 
 
 // FUNCTIONS
