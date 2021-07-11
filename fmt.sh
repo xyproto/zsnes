720531e8204d3e956b2244ae28a70b69a5d352fc
Alexander F. Rødseth <rodseth@gmail.com>
2021-07-11 16:27:41 +0200

    Update source code formatting script

--- fmt.sh
+++ fmt.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-find . -name "*.c" -exec clang-format -style=file -i {} \;
-find . -name "*.h" -exec clang-format -style=file -i {} \;
-find . -name "*.cpp" -exec clang-format -style=file -i {} \;
+find . -path ./cpu -prune -false -o -name "*.c" -exec clang-format -style=file -i {} \;
+find . -path ./cpu -prune -false -o -name "*.h" -exec clang-format -style=file -i {} \;
+find . -path ./cpu -prune -false -o -name "*.cpp" -exec clang-format -style=file -i {} \;
