0d31f916de5251a6989c4aa62565969df2038a85
Alexander F. Rødseth <rodseth@gmail.com>
2026-02-17 16:10:51 +0100

    Revert "Possible fix for #30"
    This reverts commit 7eaf2ee6c7d122bb31ea0ebf7d1321d38a2eca44.

--- cpu/execute.h
+++ cpu/execute.h
@@ -14,7 +14,7 @@ extern u1 NoSoundReinit;
 extern u1 SPCKeyPressed;
 extern u1 SSKeyPressed;
 extern u1 exiter;
-extern u4 nextframe; // tick count for timer
+extern u1 nextframe; // tick count for timer
 extern u1 pdh;
 extern u1 pressed[256 + 128 + 64]; // keyboard pressed keys in scancode
 extern u1 romloadskip;
