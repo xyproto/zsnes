3c9440a9f1d8f984691b41f6d2c0771ef1becf86
Alexander F. Rødseth <rodseth@gmail.com>
2026-02-25 10:28:19 +0100

    Format code with clang-format

--- test/zstest.h
+++ test/zstest.h
@@ -4,49 +4,52 @@
 #include <stdio.h>
 #include <string.h>
 
-static int zt_passes   = 0;
+static int zt_passes = 0;
 static int zt_failures = 0;
 static int zt_section_fails = 0;
 
-#define ZT_SECTION(name) \
-    do { printf("  %s\n", name); zt_section_fails = zt_failures; } while (0)
+#define ZT_SECTION(name)                \
+    do {                                \
+        printf("  %s\n", name);         \
+        zt_section_fails = zt_failures; \
+    } while (0)
 
-#define ZT_CHECK(expr) \
-    do { \
-        if (expr) { \
-            zt_passes++; \
-        } else { \
+#define ZT_CHECK(expr)                                                            \
+    do {                                                                          \
+        if (expr) {                                                               \
+            zt_passes++;                                                          \
+        } else {                                                                  \
             fprintf(stderr, "    FAIL [%s:%d]: %s\n", __FILE__, __LINE__, #expr); \
-            zt_failures++; \
-        } \
+            zt_failures++;                                                        \
+        }                                                                         \
     } while (0)
 
-#define ZT_CHECK_STR(got, expected) \
-    do { \
-        const char *_g = (got), *_e = (expected); \
-        if (strcmp(_g, _e) == 0) { \
-            zt_passes++; \
-        } else { \
+#define ZT_CHECK_STR(got, expected)                                            \
+    do {                                                                       \
+        const char *_g = (got), *_e = (expected);                              \
+        if (strcmp(_g, _e) == 0) {                                             \
+            zt_passes++;                                                       \
+        } else {                                                               \
             fprintf(stderr, "    FAIL [%s:%d]: got \"%s\", expected \"%s\"\n", \
-                    __FILE__, __LINE__, _g, _e); \
-            zt_failures++; \
-        } \
+                __FILE__, __LINE__, _g, _e);                                   \
+            zt_failures++;                                                     \
+        }                                                                      \
     } while (0)
 
-#define ZT_CHECK_INT(got, expected) \
-    do { \
-        int _g = (int)(got), _e = (int)(expected); \
-        if (_g == _e) { \
-            zt_passes++; \
-        } else { \
+#define ZT_CHECK_INT(got, expected)                                    \
+    do {                                                               \
+        int _g = (int)(got), _e = (int)(expected);                     \
+        if (_g == _e) {                                                \
+            zt_passes++;                                               \
+        } else {                                                       \
             fprintf(stderr, "    FAIL [%s:%d]: got %d, expected %d\n", \
-                    __FILE__, __LINE__, _g, _e); \
-            zt_failures++; \
-        } \
+                __FILE__, __LINE__, _g, _e);                           \
+            zt_failures++;                                             \
+        }                                                              \
     } while (0)
 
-#define ZT_RESULTS() \
-    do { \
+#define ZT_RESULTS()                                                \
+    do {                                                            \
         printf("\n%d passed, %d failed\n", zt_passes, zt_failures); \
-        return zt_failures ? 1 : 0; \
+        return zt_failures ? 1 : 0;                                 \
     } while (0)
