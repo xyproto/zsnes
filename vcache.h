74509ed2fc45f3ad0189405fa5a10a69159f6296
Alexander F. Rødseth <rodseth@gmail.com>
2026-06-24 12:36:02 +0200

    Fix extern declarations

--- vcache.h
+++ vcache.h
@@ -15,25 +15,25 @@ extern void cachetile8b(u4 eax);
 extern void cachetile8b16x16(u4 eax);
 extern void processsprites();
 
-extern u1 lineleft;
+extern u4 lineleft;
 extern u1 res480;
 extern u1 res640;
 extern u1 sprprifix;
-extern u1 videotroub;
+extern u4 videotroub;
 extern u2 dcolortab[2][256];
-extern u2 vesa2_bfull; // red max (or bit*1Fh)
-extern u2 vesa2_btrcl; // red transparency clear     (bit+4)
-extern u2 vesa2_btrcla; // red transparency (AND) clear (not(bit+4))
-extern u2 vesa2_clbit; // clear all bit 0's if AND is used
-extern u2 vesa2_gfull; // red max (or bit*1Fh)
-extern u2 vesa2_gtrcl; // red transparency clear     (bit+4)
-extern u2 vesa2_gtrcla; // red transparency (AND) clear (not(bit+4))
-extern u2 vesa2_rfull; // red max (or bit*1Fh)
-extern u2 vesa2_rtrcl; // red transparency clear     (bit+4)
-extern u2 vesa2_rtrcla; // red transparency (AND) clear (not(bit+4))
-extern u2 vesa2_usbit; // Unused bit in proper bit location
-extern u2 vesa2_x; // Desired screen width
-extern u2 vesa2_y; // Height
+extern u4 vesa2_bfull; // red max (or bit*1Fh)
+extern u4 vesa2_btrcl; // red transparency clear     (bit+4)
+extern u4 vesa2_btrcla; // red transparency (AND) clear (not(bit+4))
+extern u4 vesa2_clbit; // clear all bit 0's if AND is used
+extern u4 vesa2_gfull; // red max (or bit*1Fh)
+extern u4 vesa2_gtrcl; // red transparency clear     (bit+4)
+extern u4 vesa2_gtrcla; // red transparency (AND) clear (not(bit+4))
+extern u4 vesa2_rfull; // red max (or bit*1Fh)
+extern u4 vesa2_rtrcl; // red transparency clear     (bit+4)
+extern u4 vesa2_rtrcla; // red transparency (AND) clear (not(bit+4))
+extern u4 vesa2_usbit; // Unused bit in proper bit location
+extern u4 vesa2_x; // Desired screen width
+extern u4 vesa2_y; // Height
 extern u4 OMBGTestVal;
 extern u4 ngptrdat2;
 extern u4 ofshvaladd;
