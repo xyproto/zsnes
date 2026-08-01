440d9a332eb83a8d978279b4a0e4ce17c2f54ae4
Alexander F. Rødseth <rodseth@gmail.com>
2026-08-01 11:01:28 +0200

    Update clang-format related files

--- fmt.sh
+++ fmt.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-find . -path ./cpu -prune -false -o -name "*.c" -exec clang-format -style=file -i {} \;
-find . -path ./cpu -prune -false -o -name "*.h" -exec clang-format -style=file -i {} \;
-find . -path ./cpu -prune -false -o -name "*.cpp" -exec clang-format -style=file -i {} \;
+# Format the project's C sources in place.
+set -e
+cd "$(dirname "$0")"
+git ls-files -z '*.c' '*.h' '*.cpp' | xargs -0 clang-format -style=file -i
