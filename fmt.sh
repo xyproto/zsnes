0cb03f9fa6f75af784e94651e6dd1c26f0ab72a0
Alexander F. Rødseth <rodseth@gmail.com>
2026-08-18 19:06:27 +0200

    Skip test/difftest_op.c when formatting

--- fmt.sh
+++ fmt.sh
@@ -2,4 +2,5 @@
 # Format the project's C sources in place.
 set -e
 cd "$(dirname "$0")"
-git ls-files -z '*.c' '*.h' '*.cpp' | xargs -0 clang-format -style=file -i
+git ls-files '*.c' '*.h' '*.cpp' | grep -v test/difftest_op.c | xargs clang-format -style=file -i
+#git ls-files -z '*.c' '*.h' '*.cpp' | xargs -0 clang-format -style=file -i
