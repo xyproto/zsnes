8fa0431a044101f9e8616262798b60b7ba353f70
Alexander F. Rødseth <rodseth@gmail.com>
2026-08-18 23:05:17 +0200

    Ignore another file when formatting code

--- fmt.sh
+++ fmt.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
 cd "$(dirname "$0")"
-git ls-files '*.c' '*.h' '*.cpp' | \
-  grep -v test/difftest_op.c | \
-  grep -v zloader.c | \
-  xargs clang-format -style=file -i
+git ls-files '*.c' '*.h' '*.cpp' \
+  | grep -v test/difftest_op.c \
+  | grep -v zloader.c \
+  | grep -v test/ng2_harness.h \
+  | xargs clang-format -style=file -i
 
 #git ls-files -z '*.c' '*.h' '*.cpp' | xargs -0 clang-format -style=file -i
