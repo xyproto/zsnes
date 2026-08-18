ba796fd262c9e528759f5df9360e51b73ed3b3d1
Alexander F. Rødseth <rodseth@gmail.com>
2026-08-18 20:59:31 +0200

    Also skip zloader.c when formatting sources

--- fmt.sh
+++ fmt.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-# Format the project's C sources in place.
 set -e
 cd "$(dirname "$0")"
-git ls-files '*.c' '*.h' '*.cpp' | grep -v test/difftest_op.c | xargs clang-format -style=file -i
+git ls-files '*.c' '*.h' '*.cpp' | \
+  grep -v test/difftest_op.c | \
+  grep -v zloader.c | \
+  xargs clang-format -style=file -i
+
 #git ls-files -z '*.c' '*.h' '*.cpp' | xargs -0 clang-format -style=file -i
