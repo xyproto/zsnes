78569dd5c4e0cc21a3aaa75b69f24b96f55f8873
Alexander F. Rødseth <rodseth@gmail.com>
2026-09-07 16:04:02 +0200

    Include types.h + minor changes

--- zstate.h
+++ zstate.h
@@ -5,23 +5,19 @@
 #include <stdint.h>
 #include <time.h>
 
+#include "types.h"
+
 int zst_exists(void);
 void BackupSystemVars(void);
 void LoadSecondState(void);
 void RestoreSystemVars(void);
 void SaveSecondState(void);
 void SaveSramData(void);
+void loadstate(void);
 void loadstate2(void);
+void savespcdata(void);
 void statesaver(void);
 void zst_determine_newest(void);
-
-void statesaver(void);
-
-void loadstate(void);
-
-void SaveSramData(void);
-
-void savespcdata(void);
 
 extern time_t newestfiledate;
 extern uint32_t current_zst;
