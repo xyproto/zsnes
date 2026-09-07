95a68722874d724bf4d5fd94315b5fd87831036e
Alexander F. Rødseth <rodseth@gmail.com>
2026-09-07 16:02:04 +0200

    Add defines

--- gblvars.h
+++ gblvars.h
@@ -20,6 +20,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
+#ifndef GBLVARS_H
+#define GBLVARS_H
 #include "types.h"
 #include <stdbool.h>
 #include <stdint.h>
@@ -56,3 +58,5 @@ extern bool C4Enable, SFXEnable, SA1Enable, SPC7110Enable, SETAEnable, DSP4Enabl
 extern int16_t C4WFXVal, C41FXVal, Op00Multiplicand, Op04Angle, Op08X, Op18X;
 extern int16_t Op28X, Op0CA, Op02FX, Op0AVS, Op06X, Op01m, Op0DX, Op03F, Op14Zr;
 extern int16_t Op0EH, Op10Coefficient;
+
+#endif
