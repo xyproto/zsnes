d8e6c063e22a88a31c959e56e5c600dc895d98cf
Alexander F. Rødseth <rodseth@gmail.com>
2026-09-26 14:33:45 +0200

    Improve the "make fmt" script

--- fmt.sh
+++ fmt.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 set -e
 cd "$(dirname "$0")"
-git ls-files '*.c' '*.h' '*.cpp' \
-  | grep -v test/difftest_op.c \
-  | grep -v main.c \
-  | xargs clang-format -style=file -i
-
-#| grep -v test/ng2_harness.h \
-#git ls-files -z '*.c' '*.h' '*.cpp' | xargs -0 clang-format -style=file -i
+git ls-files -z '*.c' '*.h' '*.cpp' | xargs -0 clang-format -style=file -i
